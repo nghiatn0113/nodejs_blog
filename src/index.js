207edb5d185fb1a055adbbe30e7d329aa803f1fe
Nghia <tnghia.nguyen0113@gmail.com>
2023-01-11 20:32:54 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public/')));
 
+app.use(express.urlencoded({
+	extended: true
+}));
+
+app.use(express.json());
+
 // HTTP logger
 // app.use(morgan('combined'));
 
@@ -15,22 +21,31 @@ app.use(express.static(path.join(__dirname, 'public/')));
 app.engine('hbs', handlebars.engine({
 	extname: '.hbs'
 }));
+
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
 	res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
 	console.log(req.query.q);
 	res.render('news');
-})
+});
 
 app.get('/search', (req, res) => {
 	res.render('search');
 });
 
+app.post('/search', (req, res) => {
+
+	console.log(req.body);
+
+	res.send('');
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
-})
+});
