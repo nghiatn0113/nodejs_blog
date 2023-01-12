46e03ab4c592b0639f2ed0967970b622b1b55664
Nghia <tnghia.nguyen0113@gmail.com>
2023-01-12 13:22:27 +0700

    Routes and Controller

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const morgan = require('morgan');
+const route = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -26,25 +27,8 @@ app.set('view engine', 'hbs');
 
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-	res.render('home');
-});
-
-app.get('/news', (req, res) => {
-	console.log(req.query.q);
-	res.render('news');
-});
-
-app.get('/search', (req, res) => {
-	res.render('search');
-});
-
-app.post('/search', (req, res) => {
-
-	console.log(req.body);
-
-	res.send('');
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
