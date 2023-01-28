02942e1c3d378226e53b5e8ba6b6b543353b1be3
Nghia <tnghia.nguyen0113@gmail.com>
2023-01-28 18:29:49 +0700

    Done delete course task

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -49,6 +49,15 @@ class CourseController {
             })
             .catch(next);
     }
+
+    // [DELETE] /courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back');
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController;
--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
+
 async function connect() {
+    mongoose.set('strictQuery', true);
     try {
         await mongoose.connect('mongodb://localhost:27017/f8_education_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useCreateIndex: true,
         });
         console.log('Successfully connected');
     } catch (error) {
